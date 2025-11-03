62f6281c60fcbc280548baccaf8e74d752aa8a2e
serahadam <sadam3994@sdsu.edu>
2025-11-02 22:32:43 -0800

    Update heap.h

--- heap.h
+++ heap.h
@@ -15,21 +15,58 @@ struct MinHeap {
     MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap()
+       data[size] = idx;
+        upheap ( size , weightArr ) 
+            size++;
     }
 
     int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        if ( size == 0 ) {
+            cerr << "heap is empty!\n"
+                return -1;
+    }
+        int root = data[0];
+        data[0] = data[size - 1];
+        size--;
+
+    if ( size > 0 ) 
+        downheap ( 0, weightArr); 
+
+    return root; 
     }
 
     void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
-    }
+        while upheap( int pos, int weightArr[]) { 
+            while ( pos > 0 ) { 
+                int parent = ( pos - 1 ) / 2;
+            if (weightArr[data[pos]] < weightArr[data[parent]]) {
+                swap (data[pos], data[parent]);
+                    pos = parent;
+    } else {
+                break;
+            }
+     }
+}
 
     void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+      while (true) {
+          int left = 2 * pos + 1;
+          int right = 2 * pos +2;
+          int smallest = pos; 
+
+    if (left < size && weightArr[data[left]] < weightArr[data[smallest]])
+        smallest = left; 
+
+    if (right < size && weightArr[data[right]] < weightArr[data[smallest]])
+        smallest = right;
+
+    if (smallest !=pos) { 
+        swap (data[pos], data[smallest]);
+        pos = smallest;
+    } else {
+        break;
+            }
+        }
     }
 };
 

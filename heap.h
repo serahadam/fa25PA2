99e9fb297ae92434ae86f708542b69c25a8953b8
serahadam <sadam3994@sdsu.edu>
2025-11-03 21:31:53 -0800

    Update heap.h

--- heap.h
+++ heap.h
@@ -10,27 +10,27 @@
 using namespace std;
 
 struct MinHeap {
-    int data[64];
-    int size;
+    int data[64]; // stores guide of nodes 
+    int size; // number of elements within heap 
 
     MinHeap() { size = 0; }
 
 
 void upheap(int pos, int weightArr[]) { 
             while ( pos > 0 ) { 
-                int parent = ( pos - 1 ) / 2;
+                int parent = ( pos - 1 ) / 2; // parent index 
             if (weightArr[data[pos]] < weightArr[data[parent]]) {
                 swap (data[pos], data[parent]);
                     pos = parent;
-           } else break;
+           } else break; // stops if heap property is restored 
      }
 }
 
     void downheap(int pos, int weightArr[]) {
       while (true) {
-          int left = 2 * pos + 1;
-          int right = 2 * pos +2;
-          int smallest = pos; 
+          int left = 2 * pos + 1; // left child index 
+          int right = 2 * pos +2; // right child index 
+          int smallest = pos; // assumes current is smallest
 
     if (left < size && weightArr[data[left]] < weightArr[data[smallest]])
         smallest = left; 
@@ -47,8 +47,8 @@ void upheap(int pos, int weightArr[]) {
 
  void push(int idx, int weightArr[]) { 
      data[size] = idx; 
-     upheap ( size , weightArr ) 
-         size++; 
+     upheap ( size , weightArr ) // fix position 
+         size++; // increase heap size 
        }
 
     int pop(int weightArr[]) {
@@ -56,12 +56,12 @@ void upheap(int pos, int weightArr[]) {
             cerr << "heap is empty!\n"
                 return -1;
     }
-        int root = data[0];
-        data[0] = data[size - 1];
-        size--;
+        int root = data[0]; // smallest index 
+        data[0] = data[size - 1]; // move last element to root 
+        size--; // reduce size 
     if ( size > 0 ) 
-        downheap ( 0, weightArr); 
-    return root; 
+        downheap ( 0, weightArr); // restore heap property 
+    return root; // return smallest index 
     }
 };
 
